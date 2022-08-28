426990d6dfa180cf45adcb6b480cac044ebbeaa8
ht13156653635hj <112313753+ht13156653635hj@users.noreply.github.com>
2022-08-28 18:10:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -212,7 +212,7 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
     month = localtime().tm_mon
-    day = localtime().tm_mday
+    day = localtime().tm_mday + 1
     today = datetime.date(datetime(year=year, month=month, day=day))
     week = week_list[today.isoweekday() % 7]
     # 获取在一起的日子的日期格式
